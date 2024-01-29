30f380579fb171c148ba735f23c51cc7cd456b5a
OfriShaiman <nardos139@gmail.com>
2024-01-30 00:53:14 +0200

    first commit

---
+++ MAIN.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector(".nav_menu");
+    const darkModeToggle = document.querySelector(".dark-mode-toggle");
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('display-menu');
+    });
+
+    darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+});
